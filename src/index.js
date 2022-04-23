62cd3d4edd3d1b260555719ff44e84bbec0430a7
wansong <betterofsong@gmail.com>
2022-04-23 13:31:09 +0800

    update local binding port to 443

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 443;
 
 http.createServer(function(request, response) {
   response.writeHead(200, { 'Content-Type': 'text/plain' });
